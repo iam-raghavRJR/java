ea5e9f96d98a2d664be719559147addb2fca0f8e
iam-raghavRJR <72161435+iam-raghavRJR@users.noreply.github.com>
2020-10-01 00:22:54 +0530

    Create good.java

---
+++ good.java
@@ -0,0 +1,18 @@
+public class sortarray2 {
+    static boolean sort(int arr[]){
+      for(int i=0;i<arr.length-1;i++){
+          if(arr[i]>arr[i+1])
+          return false;
+
+      }
+      return true;
+    };
+    public static void main(String[] args) {
+        int arr[]={23,34,45,78,64};
+        boolean x=sort(arr);
+        System.out.println(x);
+    }
+}
+//this is also sorting algo in array it best alo in terns of time complexicty and space asd
+// its complexicty is 0(n);
+//as we go in the man havig  a lot int thsi man static class so it eisfi is a good temperature in the evenning a lot in this man having lot in this man having a goo
